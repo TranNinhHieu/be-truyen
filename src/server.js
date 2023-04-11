002b816364ed23c8f24d4c23cd79caa2952a44ca
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2023-04-11 08:48:45 +0700

    asd

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ const bootServer = () => {
             credentials: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             origin: [
-                // 'https://be-truyenh-tranh.onrender.com/',
+                'https://be-comic.onrender.com',
                 'http://localhost:3000',
                 'https://comic-riverdev-web.web.app',
                 'http://localhost:8080']
@@ -46,7 +46,7 @@ const bootServer = () => {
             credentials: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             origin: [
-                // 'https://be-truyenh-tranh.onrender.com/',
+                'https://be-comic.onrender.com',
                 'http://localhost:3000',
                 'https://comic-riverdev-web.web.app',
                 'http://localhost:5000']
