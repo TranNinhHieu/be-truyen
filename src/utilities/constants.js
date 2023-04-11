e0ab0a9a60bbd43690c0997ebcabb48c2ccfe304
TranNinhHieu <82253247+TranNinhHieu@users.noreply.github.com>
2023-04-11 08:36:36 +0700

    oke

---
+++ src/utilities/constants.js
@@ -0,0 +1,17 @@
+export const HttpStatusCode = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER: 500,
+}
+
+export const WHITELIST_DOMAINS = [
+    'http://localhost:8080',
+    'http://localhost:3000',
+    'https://comic-riverdev-web.web.app',
+    'https://comic-riverdev-web.firebaseapp.com',
+    'https://comic-riverdev-api.herokuapp.com'
+]
